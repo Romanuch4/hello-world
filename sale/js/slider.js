956ee77c270377fe8d89c5accb2b511604cbf8ca
Roman <pomankrentovsky@gmail.com>
2019-10-12 12:26:31 +0300

    v1.3

---
+++ sale/js/slider.js
@@ -0,0 +1,26 @@
+(function() {
+  const moveLeft = document.querySelector('.main-img-content-left'),
+        container = document.querySelector('.main-img-container'),
+        moveRight = document.querySelector('.main-img-content-right');
+  let counter = 0;
+
+  moveLeft.addEventListener('click', function() {
+    if(counter == 0) {
+      counter = - 300;
+      container.style.left = counter + '%';
+    } else {
+      counter+=100;
+      container.style.left = counter + '%';
+    }
+  });
+
+  moveRight.addEventListener('click', function() {
+    if(counter == -300) {
+      counter = 0
+      container.style.left = counter + '%';
+    } else {
+      counter-=100;
+      container.style.left = counter + '%';
+    }
+  });
+})();
