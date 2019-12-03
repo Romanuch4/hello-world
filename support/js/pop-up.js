d0ef57119923200d1385ff3eac73730eba262b83
Roman <pomankrentovsky@gmail.com>
2019-12-03 20:57:39 +0200

    Обработчик на кнопку "Оценить"

---
+++ support/js/pop-up.js
@@ -0,0 +1,6 @@
+(function () {
+  const button = document.querySelector('.main-button');
+  button.addEventListener('click', function () {
+    window.popUpThanks.style.display = 'flex';
+  });
+})();
